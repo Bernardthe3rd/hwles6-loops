9b15607defee8087dd622950df11eb63cd7b1270
Bernardthe3rd <bammeijer@outlook.com>
2023-12-27 13:49:23 +0100

    started and finished loops and arrays

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -11,10 +11,10 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // console.log(names) geeft: ["Henk", "Piet", "Fred", "Joop"]
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
-
-
-
-
+console.log(names)
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i] + 'je');
+}
 
 // ==========================================
 // Opdracht 2
@@ -29,10 +29,12 @@ const numbers = [2, 4, 5, 29, 38];
 // console.log(numbers) geeft: [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 4, 8, 15, 87, 76 ];
-
-
-
-
+console.log(numbers);
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i]%2===0) {
+        console.log(numbers[i]*2)
+    } else console.log(numbers[i]*3)
+}
 
 // ==========================================
 // Opdracht 3
@@ -56,6 +58,8 @@ const squares = [30, 2, 8, 24, 11];
 //   'Het volume van 24 is 13824',
 //   'Het volume van 11 is 1331'
 // ]
-
-
-
+console.log(squares);
+for (let i = 0; i < squares.length; i++) {
+    const volume = squares[i]*squares[i]*squares[i];
+    console.log('Het volume van '+squares[i]+' is '+volume);
+}
